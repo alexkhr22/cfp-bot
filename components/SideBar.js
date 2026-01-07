1505f121ee9086f230cd561c2b984e5b09eafe8e
Hendrik Martin <hendrikmartin3@web.de>
2026-01-07 12:55:49 +0100

    added tags list

--- components/SideBar.js
+++ components/SideBar.js
@@ -3,10 +3,29 @@ import config from "@/config";
 const SideBar = () =>{
     return (
     
-    <div class="rechteck-sidebar">
-        <button class="btn-home">Home</button>
-        <button class="btn-newgrp">Neue Gruppe</button>
-        <button class="btn-grps">Gruppen</button>
+    <div className="rechteck-sidebar">
+        <button className="sidebar-btn">Home</button>
+        <div className="tagsp-div">
+            <p className="tags-p">Meine Tags:</p>
+        </div>
+        <div className="home-dropdown">
+            <ul className="scroll-list">
+                <li>Eintrag 1</li>
+                <li>Eintrag 2</li>
+                <li>Eintrag 3</li>
+                <li>Eintrag 4</li>
+                <li>Eintrag 5</li>
+                <li>Eintrag 6</li>
+                <li>Eintrag 7</li>
+                <li>Eintrag 8</li>
+            </ul>
+        </div>
+        <div className="newtag-div">
+            <input className="tag-input" type="text" placeholder="Neuer Tag" />
+            <button className="newtag-btn">+</button>
+        </div>
+        <button className="sidebar-btn">Neue Gruppe</button>
+        <button className="sidebar-btn">Gruppen</button>
     </div>
         
     ); 

2ae28c2061adbba40df25e4e155cadd5b5cdde91
Hendrik Martin <hendrikmartin3@web.de>
2026-01-08 16:26:25 +0100

    progress on adding cfps manually

--- components/LeftSideHome.js
+++ components/LeftSideHome.js
@@ -4,18 +4,60 @@ import config from "@/config";
 import { useState } from "react";
 
 const LeftSideHome = () =>{
+    const [showAddCfp, setShowAddCfp] = useState(false);
+
+
     return (
         <div className="rechteck-leftside">
             <header className="cfp-header">
                 <div className="add-cfp">
                     <p className="cfp-title">CfP's</p>
-                    <button className="addcfp-btn">+</button>
+                    <button className="addcfp-btn" onClick={() => setShowAddCfp(true)}>+</button>
                 </div>
                 <div className="search-wrapper">
                     <input type="search" className="cfp-searchbar" placeholder="Suchen …"></input>
                     <button type="submit">🔍</button>
                 </div>
             </header>
+            {showAddCfp && (
+                <div className="addcfp-div">
+                    <header>CfP hinzufügen</header>
+                    <ul className="addcfp-list">
+                        <li>
+                            <label>Titel:</label>
+                            <input />
+                        </li>
+                        <li>
+                            <label>Einreichungs Deadline:</label>
+                            <input />
+                        </li>
+                        <li>
+                            <label>Ort:</label>
+                            <input />
+                        </li>
+                        <li>
+                            <label>Datum der Konferenz:</label>
+                            <input />
+                        </li>
+                        <li>
+                            <label>URL:</label>
+                            <input />
+                        </li>
+                        <li>
+                            <label>Datum Rückmeldung:</label>
+                            <input />
+                        </li>
+                        <li>
+                            <label>Einreichungsform:</label>
+                            <input />
+                        </li>
+                        <li>
+                            <label>Wort-/Seitenbeschränkung:</label>
+                            <input />
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
